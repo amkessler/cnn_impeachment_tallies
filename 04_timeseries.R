a4afb5768066a546109c5ff0e3cf5bdf4073875e
amkessler <kessler.aaron@gmail.com>
2019-10-03 18:12:22 -0400

    first time series

--- 04_timeseries.R
+++ 04_timeseries.R
@@ -11,3 +11,28 @@ sept_daily_allyes <- working_joined %>%
   count(date_exact) %>% 
   arrange(date_exact)
 
+
+# some playing around with tibbletime 
+# https://business-science.github.io/tibbletime/articles/TT-04-use-with-dplyr.html
+
+series <- create_series('2013' ~ '2017', 'daily', class = "Date") %>%
+  mutate(var = rnorm(n()))
+
+series
+
+#traditional through group by
+series %>%
+  mutate(year = year(date), month = month(date)) %>%
+  group_by(year, month) %>%
+  summarise(mean_var = mean(var))
+
+
+# With tibbletime, rather than creating new columns to group on, you manipulate your original date column 
+# into something that corresponds to the period you are summarising at. 
+# The tibbletime way to do this is with collapse_by().
+
+series %>%
+  collapse_by("monthly") %>%
+  group_by(date) %>%
+  summarise(mean_var = mean(var))
+
